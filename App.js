2720c321a83b97a3ee58f628365952b534d319a9
Richard Hyesung O <orichh@gmail.com>
2022-01-16 22:26:26 -0500

    Add react image, rearrange files, add scrolling

--- App.js
+++ App.js
@@ -1,32 +1,56 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput, Alert } from "react-native";
-import TypeScript from "./TypeScript";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Alert,
+  Image,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
+import TypeScript from "./components/TypeScript.tsx";
+
+const image = { uri: "https://reactnative.dev/img/tiny_logo.png" };
 
 export default function App() {
   const [names, setNames] = useState(["Richard"]);
   const [counter, setCounter] = useState(0);
   const [userInput, setUserInput] = useState("testing");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! helllloooo</Text>
-      {names.map((num, idx) => {
-        return <Text key={idx}>{num}</Text>;
-      })}
-      <TypeScript names={names} />
-      <StatusBar style="auto" />
-      <Text>{counter}</Text>
-      <Button
-        title="Press me"
-        onPress={() => {
-          setCounter(counter + 1);
-        }}
-      ></Button>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        defaultValue={userInput}
-      ></TextInput>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.container}>
+          <Image
+            source={image}
+            resizeMode="cover"
+            style={{ width: 50, height: 50 }}
+          ></Image>
+          <Text>
+            Open up App.js to start working on your app! helllloooo!!!
+          </Text>
+          {names.map((num, idx) => {
+            return <Text key={idx}>{num}</Text>;
+          })}
+          <TypeScript names={names} />
+          <StatusBar style="auto" />
+          <Text>{counter}</Text>
+          <Button
+            title="Press me"
+            onPress={() => {
+              setCounter(counter + 1);
+            }}
+          ></Button>
+          <TextInput
+            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+            defaultValue={userInput}
+          ></TextInput>
+          <Text>reset</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -35,6 +59,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "center",
+    paddingTop: StatusBar.currentHeight,
+  },
+  image: {
+    flex: 1,
     justifyContent: "center",
   },
 });
